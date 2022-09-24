c2a8c2b249c2c4232487aadbf133547a0c3fbe14
Chris Williamson <chriswill0629@gmail.com>
2022-09-24 03:12:18 -0400

    finished cart view

--- src/components/navBar/cartView.jsx
+++ src/components/navBar/cartView.jsx
@@ -25,6 +25,23 @@ const Overlay = styled.div`
 	z-index: 0;
 	transition: all 0.5s ease-in-out;
 `;
+const ProductCard = () => {
+	return (
+		<div className="productCard">
+			<div className="productName">Red Dragon Helmet</div>
+			<div className="productSku">SKU: 10266606</div>
+			<div className="productSC">Size: 3XL</div>
+			<div className="productSC">Color: Black</div>
+			<div className="productVar">
+				<div className="productQuantity bg-primary fw-bold">
+					<button className="Bleft fw-bold">-</button>
+					<div className="qty">asdf</div>
+					<button className="BRight fw-bold">+</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default function CartViewRender({ cartView, setCartView, size }) {
 	return (
@@ -42,8 +59,29 @@ export default function CartViewRender({ cartView, setCartView, size }) {
 							<span>View Wishlist</span>
 						</div>
 					</div>
-					<div className="checkout">CHECKOUT</div>
-					<div className="mobiCartProducts">asdf</div>
+					<div className="checkout bg-primary">CHECKOUT</div>
+					<div className="mobiCartProducts">
+						<div className="emptyCart">
+							<p>No items in cart</p>
+						</div>
+						<ProductCard />
+						<div className="cartSpacer"></div>
+						<div className="mobiTotalView">
+							<div className="mobiTotal halfBorder">
+								<div className="mobiTotalLeft">Taxes:</div>
+								<div className="mobiTotalRight">$20.99</div>
+							</div>
+							<div className="mobiTotal halfBorder">
+								<div className="mobiTotalLeft">Shipping:</div>
+								<div className="mobiTotalRight">$7.00</div>
+							</div>
+							<div className="mobiTotal halfBorder">
+								<div className="mobiTotalLeft">Total:</div>
+								<div className="mobiTotalRight">$27.99</div>
+							</div>
+						</div>
+					</div>
+					<div className="emptyOut bg-danger">EMPTY CART</div>
 				</div>
 			</SideBarNav>
 		</div>
